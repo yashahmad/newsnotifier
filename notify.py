80ec7ec41f86acd4e31b5b96327c7af8359a1f7c
Yash Ahmad <yash.ahd786@gmail.com>
2018-06-17 23:21:18 -0700

    Add files via upload

---
+++ notify.py
@@ -0,0 +1,18 @@
+import time
+import notify2
+from topnews import topStories
+
+ICON_PATH="put full path to icon image here"
+
+newsitems = topStories()
+notify2.init("News Notifier")
+
+n=notify2.Notification(None, icon=ICON_PATH)
+n.set_urgency(notify2.URGENCY_NORMAL)
+
+n.set_timeout(10000)
+
+for newsitem in newsitems:
+    n.update(newsitem['title'],newsitem['description'])
+    n.show()
+    time.sleep(15)
---
+++ topnews.py
@@ -0,0 +1,26 @@
+import requests
+import xml.etree.ElementTree as ET
+
+RSS_FEED_URL ="http://www.hindustantimes.com/rss/topnews/rssfeed.xml"
+
+def loadRSS():
+    resp = requests.get(RSS_FEED_URL)
+    return resp.content
+
+def parseXML(rss):
+    root = ET.fromstring(rss)
+    newsitems=[]
+    for item in root.findall('./channel/item'):
+        news={}
+        for child in item:
+            if child.tag =="{http://search.yahoo.com/mrss/}content":
+                news['media'] = child.attrib['url']
+            else:
+                news[child.tag]=child.text.encode('utf8')
+        newsitems.append(news)
+    return newsitems
+
+def topStories():
+    rss=loadRSS()
+    newsitems=parseXML(rss)
+    return newsitems
